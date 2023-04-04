c310e8a88b907ec9738f28df33fd708b089c927e
Goce Davinovski <goce.davinovski@tarmac.io>
2023-04-04 04:54:45 +0200

    Initial terraform conifguration

---
+++ env/dev/providers.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+
+  default_tags {
+   tags = {
+     Environment = "dev"
+     Product     = "Automation-station"
+	 GithubURL   = "https://github.com/tarmac/automation-station-tf"
+   }
+ }
+}
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "s3-785700991304-terraform-state-dev"
+    dynamodb_table = "dynamo-785700991304-terraform-locks-dev"
+    encrypt        = "true"
+    key            = "infra-dev.tfstate"
+    profile        = "automation"
+    region         = "us-east-1"
+  }
+}
---
+++ env/dev/variables.tf
@@ -0,0 +1,11 @@
+variable "projectname" {
+  default = "internal"
+}
+
+variable "profile" {
+  default = "automation"
+}
+
+variable "region" {
+  default = "us-east-1"
+}
