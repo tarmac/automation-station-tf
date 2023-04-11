070d6a421272925a36da2779ed2585a98ee07d42
Branko Mandil <brankomandil@Brankos-MacBook-Pro.local>
2023-04-11 11:36:06 +0200

    additinal adding rules for CloudFront

--- modules/waf/rules.tf
+++ modules/waf/rules.tf
@@ -84,3 +84,90 @@ resource "aws_wafv2_web_acl" "common_rule_set_regional" {
     sampled_requests_enabled   = true
   }
 }
+
+resource "aws_wafv2_web_acl" "common_rule_set_cloudfront" {
+  name        = "${var.tags["env"]}-${var.tags["projectname"]}-web-acl-cloudfront"
+  description = "Rules that are generally applicable to web applications."
+  scope       = "CLOUDFRONT"
+  default_action {
+    allow {}
+  }
+  rule {
+    name     = "AWS-BotControlRuleSet"
+    priority = 0
+    override_action {
+      count {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesBotControlRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesBotControlRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+  rule {
+    name     = "AWS-CommonRuleSet"
+    priority = 1
+    override_action {
+      count {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-AWSManagedRulesCommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+  rule {
+    name     = "AWS-AmazonIpReputationList"
+    priority = 2
+    override_action {
+      count {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-ManagedRulesAmazonIpReputationList"
+      sampled_requests_enabled   = true
+    }
+  }
+  rule {
+    name     = "AWS-KnownBadInputs"
+    priority = 3
+    override_action {
+      count {}
+    }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWS-ManagedRulesKnownBadInputsRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+  tags = var.tags
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${var.tags["env"]}-${var.tags["projectname"]}-web-acl-metric"
+    sampled_requests_enabled   = true
+  }
+}
