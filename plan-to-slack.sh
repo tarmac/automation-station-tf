d2c3396e24aef215cf1bf1743182a4c5b571b479
Nikola Popovski <npopovski@spins.com>
2023-06-29 16:28:11 +0200

    final ci/cd approval stage

--- plan-to-slack.sh
+++ plan-to-slack.sh
@@ -6,10 +6,12 @@ if [[ "${planresult}" == "2" ]]
 then
    TEXT="*Terraform Plan Changed* - Please check Codebuild logs for the plan and approve or reject the deploy"
    TITLE="Changes in Terraform Plan"
-   WEBHOOK_URL="https://hooks.slack.com/services/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"
+   WEBHOOK_URL="https://hooks.slack.com/services/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"
    COLOR="RED"
    MESSAGE=$( echo ${TEXT} | sed 's/"/\"/g' | sed "s/'/\'/g" )
    JSON="{\"title\": \"${TITLE}\", \"themeColor\": \"${COLOR}\", \"text\": \"${MESSAGE}\" }"
    # Post to Slack.
    curl -H "Content-Type: application/json" -d "${JSON}" "${WEBHOOK_URL}"
    fi
+
+   #https://hooks.slack.com/services/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
