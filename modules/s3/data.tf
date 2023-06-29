384095df2984a78db5408e2cc4a9acaa2338539f
Branko Mandil <117287319+MaNDiL555@users.noreply.github.com>
2023-06-29 13:24:05 +0200

    Update data.tf

--- modules/s3/data.tf
+++ modules/s3/data.tf
@@ -28,23 +28,3 @@ data "aws_iam_policy_document" "automation_station_s3_access" {
       ]
     }
   }
-  statement {
-    
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${var.aws_acc_id}:user/branko"]
-    }
-
-    actions = [
-      "s3:PutObject",
-      "s3:ListBucket",
-      "s3:GetObject",
-      "s3:DeleteObject"
-    ]
-
-    resources = [
-      "arn:aws:s3:::${var.tags["projectname"]}-${var.s3_bucket_name}-${var.tags["env"]}",
-      "arn:aws:s3:::${var.tags["projectname"]}-${var.s3_bucket_name}-${var.tags["env"]}/*"
-    ]
-  }
-}
