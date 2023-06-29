8912b376ee1b0bfb95dceee7100c3be7e442df7e
Nikola Popovski <npopovski@spins.com>
2023-06-29 15:14:32 +0200

    add ci/cd approval stage

---
+++ plan_to_slack.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+terraform plan -detailed-exitcode
+export planresult=$?
+if [[ "${planresult}" == "2" ]]
+then
+   TEXT="*Terraform Plan Changed* - Please check Codebuild logs for the plan and approve or reject the deploy"
+   TITLE="Changes in Terraform Plan"
+   WEBHOOK_URL="#!/bin/bash
+terraform plan -detailed-exitcode
+export planresult=$?
+if [[ "${planresult}" == "2" ]]
+then
+   TEXT="*Terraform Plan Changed* - Please check Codebuild logs for the plan and approve or reject the deploy"
+   TITLE="Changes in Terraform Plan"
+   WEBHOOK_URL="https://hooks.slack.com/services/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"
+   COLOR="RED"
+   MESSAGE=$( echo ${TEXT} | sed 's/"/\"/g' | sed "s/'/\'/g" )
+   JSON="{\"title\": \"${TITLE}\", \"themeColor\": \"${COLOR}\", \"text\": \"${MESSAGE}\" }"
+   # Post to Slack.
+   curl -H "Content-Type: application/json" -d "${JSON}" "${WEBHOOK_URL}""
+   COLOR="RED"
+   MESSAGE=$( echo ${TEXT} | sed 's/"/\"/g' | sed "s/'/\'/g" )
+   JSON="{\"title\": \"${TITLE}\", \"themeColor\": \"${COLOR}\", \"text\": \"${MESSAGE}\" }"
+   # Post to Slack.
+   curl -H "Content-Type: application/json" -d "${JSON}" "${WEBHOOK_URL}"
