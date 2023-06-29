648ef66b97df14ae626793a1ad14d55c76865646
Branko Mandil <117287319+MaNDiL555@users.noreply.github.com>
2023-06-29 14:14:18 +0200

    Update s3.tf

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
 resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
   bucket = aws_s3_bucket.s3_bucket.id
   rule {
-    object_ownership = "BucketOwnerPreferred"
+    object_ownership = "ObjectWriter"
   }
 }
 resource "aws_s3_bucket_acl" "bucket_acl" {
