4e0efe81abeda987c4b4aeeba27a7590808a2322
Nikola Popovski <npopovski@spins.com>
2023-06-30 14:13:17 +0200

    change ci/cd approval stage

--- plan-to-slack.sh
+++ plan-to-slack.sh
@@ -6,7 +6,7 @@ if [[ "${planresult}" == "2" ]]
 then
    TEXT="*Terraform Plan Changed* - Please check Codebuild logs for the plan and approve or reject the deploy"
    TITLE="Changes in Terraform Plan"
-   WEBHOOK_URL="https://hooks.slack.com/services/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"
+   WEBHOOK_URL="https://hooks.slack.com/services/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"
    COLOR="RED"
    MESSAGE=$( echo ${TEXT} | sed 's/"/\"/g' | sed "s/'/\'/g" )
    JSON="{\"title\": \"${TITLE}\", \"themeColor\": \"${COLOR}\", \"text\": \"${MESSAGE}\" }"
