35ed0a93902459c8ccc64808b5ef9478e30c7173
Goce Davinovski <goce.davinovski@tarmac.io>
2023-07-06 13:42:22 +0200

    Remove profile from provider

--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -2,9 +2,9 @@ variable "projectname" {
   default = "internal"
 }
 
-variable "profile" {
-  default = "automation"
-}
+# variable "profile" {
+#   default = "automation"
+# }
 
 variable "region" {
   default = "us-east-1"
