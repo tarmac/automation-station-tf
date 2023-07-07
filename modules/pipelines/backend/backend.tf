c86c08989f977edfc602d75fdfe93a313f1d8e43
Branko Mandil <brankomandil@Brankos-MacBook-Pro.local>
2023-07-07 11:15:07 +0200

    adding resource aws_kms_key

--- modules/pipelines/backend/backend.tf
+++ modules/pipelines/backend/backend.tf
@@ -125,6 +125,7 @@ resource "aws_s3_bucket_acl" "codepipeline_bucket_acl" {
   acl    = "private"
 }
 
+resource "aws_kms_key" "s3kmskey" {}
 resource "aws_kms_alias" "s3kmskey" {
   name          = "alias/myKmsKey"
   target_key_id = aws_kms_key.s3kmskey.key_id
